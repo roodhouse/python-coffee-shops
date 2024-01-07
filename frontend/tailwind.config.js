5a0a56fa76c4e491a24fd8a56d2c1f20d216e32e
roodhouse <rughjm@gmail.com>
2024-01-07 12:40:22 -0600

    colors and fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,5 +6,15 @@ module.exports = {
   theme: {
     extend: {},
   },
+  colors: {
+    black: '#000000',
+    white: '#ffffff',
+    transparent: 'transparent',
+    gray: '#343434',
+    green: '#4CAF4F',
+    yellow: '#F6D95E',
+    red: '#FF0000',
+    blue: '#0B58CA'
+  },
   plugins: [],
 }
