ff111aa2db93fd49dd302fcd507a0729a8387771
roodhouse <rughjm@gmail.com>
2024-01-15 16:12:24 -0600

    summary done

--- frontend/src/components/store/storeSummary/StoreSummaryIcons.jsx
+++ frontend/src/components/store/storeSummary/StoreSummaryIcons.jsx
@@ -9,11 +9,44 @@ function StoreSummaryIcons() {
     const handleClick = (e) => {
         const iconParent = e.currentTarget
 
-        if ( iconParent.classList.contains('text-[#ddd]')) {
-            iconParent.classList.remove('text-[#ddd]')
-            iconParent.classList.add('text-red')
+        function getSiblings(element) {
+            const parent = element.parentNode;
+
+            const children = Array.from(parent.children)
+
+            const siblings = children.filter(child => child !== element)
+
+            return siblings
+        }
+
+        const siblings = getSiblings(iconParent)
+
+        console.log(siblings)
+        console.log(iconParent.id)
+
+        for (let i = 0; i < siblings.length; i++ ) {
+            if (siblings[i].classList.contains('text-red') || siblings[i].classList.contains('text-green') || siblings[i].classList.contains('text-yellow')) {
+                siblings[i].classList.replace('text-red', 'text-[#ddd]')
+                siblings[i].classList.replace('text-green', 'text-[#ddd]')
+                siblings[i].classList.replace('text-[#F6D95E]', 'text-[#ddd]')
+            }
+        }
+
+        if ( iconParent.classList.contains('text-[#ddd]') ) {
+            if ( iconParent.id === 'noIconContainer' ) {
+                iconParent.classList.remove('text-[#ddd]')
+                iconParent.classList.add('text-red')
+            } else if ( iconParent.id === 'yesIconContainer' ) {
+                iconParent.classList.remove('text-[#ddd]')
+                iconParent.classList.add('text-green')
+            } else {
+                iconParent.classList.remove('text-[#ddd]')
+                iconParent.classList.add('text-[#F6D95E]')
+            }
         } else {
-            iconParent.classList.remove('text-red')
+            iconParent.classList.remove(['text-red'])
+            iconParent.classList.remove(['text-green'])
+            iconParent.classList.remove(['text-[#F6D95E]'])
             iconParent.classList.add('text-[#ddd]')
         }
 
@@ -25,10 +58,10 @@ function StoreSummaryIcons() {
             <div id="noIconContainer" className='text-[#ddd]' onClick={handleClick}>
                 <FaHeartCrack />
             </div>
-            <div id="sometimesIconContainer" className='text-[#ddd]'>
+            <div id="sometimesIconContainer" className='text-[#ddd]' onClick={handleClick}>
                 <FaHeart />
             </div>
-            <div id="yesIconContainer" className='text-[#ddd]'>
+            <div id="yesIconContainer" className='text-[#ddd]' onClick={handleClick}>
                 <FaHeart />
             </div>
         </div>
