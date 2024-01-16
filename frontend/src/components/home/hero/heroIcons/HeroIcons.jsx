c64d930a870cdae0c21012f55a466200ac88fa5a
roodhouse <rughjm@gmail.com>
2024-01-16 10:17:07 -0600

    added filter color change

--- frontend/src/components/home/hero/heroIcons/HeroIcons.jsx
+++ frontend/src/components/home/hero/heroIcons/HeroIcons.jsx
@@ -5,7 +5,7 @@ import { useMain } from '../../../../context/main'
 
 function HeroIcons() {
 
-  const { toggleFilter } = useMain()
+  const { toggleFilter, filter } = useMain()
 
   const handleClick = () => {
     toggleFilter()
@@ -14,7 +14,8 @@ function HeroIcons() {
   return (
     <>
       <div id="heroIconsContainer" className='flex flex-nowrap '>
-        <div id="filterIconWrapper" className='cursor-pointer px-2 py-3 w-11 flex justify-center shadow-[0_2px_5px_0_rgba(0,0,0,0.16),0_2px_10px_0_rgba(0,0,0,0.12)] rounded-md' onClick={handleClick}>
+        {/* <div id="filterIconWrapper" className='cursor-pointer px-2 py-3 w-11 flex justify-center shadow-[0_2px_5px_0_rgba(0,0,0,0.16),0_2px_10px_0_rgba(0,0,0,0.12)] rounded-md' onClick={handleClick}> */}
+        <div id="filterIconWrapper" className={`${filter ? 'bg-[#343434] text-white' : 'bg-white text-black'} cursor-pointer px-2 py-3 w-11 flex justify-center shadow-[0_2px_5px_0_rgba(0,0,0,0.16),0_2px_10px_0_rgba(0,0,0,0.12)] rounded-md`} onClick={handleClick}>
             <FilterIcon />
         </div>
         <div id="mapIconWrapper" className='cursor-pointer px-2 py-3 w-11 flex justify-center shadow-[0_2px_5px_0_rgba(0,0,0,0.16),0_2px_10px_0_rgba(0,0,0,0.12)] rounded-md'>
