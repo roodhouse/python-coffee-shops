161a31372f8654db92eef5434e1ee4736b608208
roodhouse <rughjm@gmail.com>
2024-01-16 21:38:54 -0600

    up and running now

--- app/routes/home.py
+++ app/routes/home.py
@@ -13,3 +13,44 @@ bp = Blueprint("home", __name__, url_prefix="/")
 @bp.route("/")
 def index():
     return send_from_directory("../frontend/build", "index.html")
+
+@bp.route("/static/css/<path:filename>")
+def serve_static_css(filename):
+    return send_from_directory("../frontend/build/static/css", filename)
+
+
+@bp.route("/static/js/<path:filename>")
+def serve_static_js(filename):
+    return send_from_directory("../frontend/build/static/js", filename)
+
+
+@bp.route("/static/media/<path:filename>")
+def serve_static_media(filename):
+    return send_from_directory("../frontend/build/static/media", filename)
+
+
+@bp.route("/assets/<path:filename>")
+def serve_static_assets(filename):
+    return send_from_directory("../frontend/build/assets", filename)
+
+
+@bp.route("/manifest.json")
+def serve_manifest():
+    with current_app.open_resource("../frontend/build/manifest.json") as f:
+        mainifest_data = json.load(f)
+    return jsonify(mainifest_data)
+
+
+@bp.route("/favicon.ico")
+def serve_favicon():
+    return send_from_directory("../frontend/build", "favicon.ico")
+
+
+@bp.route("/logo192.png")
+def serve_logo192():
+    return send_from_directory("../frontend/build", "logo192.png")
+
+
+@bp.route("/logo512.png")
+def serve_logo512():
+    return send_from_directory("../frontend/build", "logo512.png")
