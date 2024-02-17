b014c1e49dcb0e10d05ff1f488a9b188fe9db3d5
roodhouse <rughjm@gmail.com>
2024-02-16 18:20:42 -0600

    seeds update

--- seeds.py
+++ seeds.py
@@ -14,7 +14,7 @@ db.add_all([
         email='rughjm@gmail.com', 
         password='12345', 
         review_ids=["Perky Beans", "Scooter\'s Coffee"],
-        avatar = '/assets/meAgain.jpeg'
+        avatar = 'http://127.0.0.1:5000/assets/meAgain.jpeg'
     ),
     Users(
         email='rooinfo@gmail.com', 
@@ -29,7 +29,7 @@ db.commit()
 db.add_all([
     Venues(
         name = 'Perky Beans',
-        image = '/assets/perkyBeans.jpeg',
+        image = 'http://127.0.0.1:5000/assets/perkyBeans.jpeg',
         location = 'https://www.google.com/maps/place/Perky+Beans+Coffee+%26+PB+Cafe/@30.6271539,-97.867737,17z/data=!3m1!4b1!4m6!3m5!1s0x865b2d6421c093a1:0xbb3c0b4e28b76730!8m2!3d30.6271493!4d-97.8651621!16s%2Fg%2F11h4pl1cck?entry=ttu',
         address = '2080 N US Hwy 183 #210, Leander, TX 78641',
         hours = [
@@ -48,7 +48,7 @@ db.add_all([
     ),
     Venues(
         name = 'Scooter\'s Coffee',
-        image = '/assets/scooters.jpeg',
+        image = 'http://127.0.0.1:5000/assets/scooters.jpeg',
         location = "https://www.google.com/maps/place/Scooter's+Coffee/@30.6029998,-97.8604322,15z/data=!4m6!3m5!1s0x865b2b4b56f3004b:0xa41ff8f6994ee64a!8m2!3d30.6029998!4d-97.8604322!16s%2Fg%2F11vbc6yn_g?entry=ttu",
         address = '3625 N US Hwy 183, Leander, TX 78641',
         hours = [
