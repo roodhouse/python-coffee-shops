c1f2c59904882eada1bed58141ff9a2c86140e7c
roodhouse <rughjm@gmail.com>
2024-03-03 15:19:58 -0600

    image added to store page

--- frontend/src/components/Store.jsx
+++ frontend/src/components/Store.jsx
@@ -4,13 +4,19 @@ import StoreStats from './store/StoreStats'
 import StoreSummary from './store/StoreSummary'
 import StoreDetails from './store/StoreDetails'
 import StoreFooter from './store/StoreFooter'
+import { useMain } from '../context/main'
 
 function Store() {
+
+    const { currentVenueData } = useMain()
+
+
 
   return (
     <>
         <div id="storeContainer">
-            <div id="storeHeroWrapper" className='bg-[url("assets/perkyBeans.jpeg")] bg-[rgba(0,0,0,0.5)] bg-blend-overlay h-96 bg-cover bg-no-repeat flex items-end mb-9'>
+            {/* <div id="storeHeroWrapper" className='bg-[url("assets/perkyBeans.jpeg")] bg-[rgba(0,0,0,0.5)] bg-blend-overlay h-96 bg-cover bg-no-repeat flex items-end mb-9'> */}
+            <div id="storeHeroWrapper" style={{ backgroundImage: currentVenueData && currentVenueData.image ? `url(${currentVenueData.image})` : 'none' }} className='bg-[rgba(0,0,0,0.5)] bg-blend-overlay h-96 bg-cover bg-no-repeat flex items-end mb-9'>
                 <StoreHero />
             </div>
             <div id="storeMainContainer" className='px-[.75rem]'>
