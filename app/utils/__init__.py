f8fa9015794b581712d8075bcd33f06628780fd5
roodhouse <rughjm@gmail.com>
2024-03-18 19:48:04 -0500

    restored code that was working

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,2 +1,4 @@
 from .auth import token_required as token_required
 from .process_image import process_image as process_image
+from .city_state_data import city_state_data as city_state_data
+from .city_state_data import update_city_state_data as update_city_state_data
